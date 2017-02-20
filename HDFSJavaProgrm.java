216c9cb56ea4d3975689d28ee2eb88f2928ffb0b
rohitj94 <rohitj94@gmail.com>
2017-02-20 23:45:36 +0530

    HDFSJava

---
+++ HDFSJavaProgrm.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import java.net.*;
+import org.apache.hadoop.fs.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileNotFoundException;
+
+public class HDFSJavaProgrm {
+
+    public static List<String> getAllFilePath(Path filePath, FileSystem fs) throws FileNotFoundException, IOException
+    {
+        List<String> fileList = new ArrayList<String>();
+       
+        FileStatus[] fileStatus = fs.listStatus(filePath);
+       
+        for (FileStatus fileStat : fileStatus) {
+            if (fileStat.isDirectory()) {
+                fileList.addAll(getAllFilePath(fileStat.getPath(), fs));
+            } else {
+                fileList.add(fileStat.getPath().toString());
+            }
+        }
+        return fileList;
+    }
+   
+    public static void main(String args[]) throws IOException, URISyntaxException
+    {
+        Configuration conf = new Configuration();
+       
+        for(int j=0; j<args.length;j++)
+        {
+            List<String> Listing = getAllFilePath(new Path(args[j]),FileSystem.get(conf));
+            for (int i=0;i < Listing.size();i++)
+            {
+              System.out.println(Listing.get(i));   
+            }
+        }
+    }
+}
